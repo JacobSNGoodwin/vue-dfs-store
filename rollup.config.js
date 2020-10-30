d2cabdb42f42fc9bd5ef3949279f991a342ca8bf
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-10-30 16:14:28 -0700

    Export types

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'output',
-    format: 'cjs',
-    sourcemap: true,
-  },
-  external: ['vue'],
-  plugins: [typescript()],
-};
+export default [
+  // {
+  //   input: 'src/index.ts',
+  //   output: {
+  //     dir: 'dist',
+  //   },
+  //   // external: ['vue'],
+  //   plugins: [typescript({ declaration: true, outDir: 'dist' })],
+  // },
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist',
+      format: 'cjs',
+    },
+    external: ['vue'],
+    plugins: [typescript()],
+  }
+];
