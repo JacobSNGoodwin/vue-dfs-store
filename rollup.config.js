b0dd5a6cccd21e06535dbe3ec028c7300897b01a
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-10-30 17:29:32 -0700

    Get modules exporting with typings

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,40 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default [
-  // {
-  //   input: 'src/index.ts',
-  //   output: {
-  //     dir: 'dist',
-  //   },
-  //   // external: ['vue'],
-  //   plugins: [typescript({ declaration: true, outDir: 'dist' })],
-  // },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        // dir: 'dist',
+        file: 'dist/index.js',
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        // dir: 'dist',
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        // dir: 'dist',
+        file: 'dist/index.iife.js',
+        format: 'iife',
+        exports: 'named',
+        name: 'vuedfs',
+        sourcemap: true,
+      },
+    ],
+    external: ['vue'],
+    plugins: [typescript()],
+  },
   {
     input: 'src/index.ts',
     output: {
       dir: 'dist',
-      format: 'cjs',
     },
-    external: ['vue'],
-    plugins: [typescript()],
-  }
+    plugins: [
+      typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' }),
+    ],
+  },
 ];
