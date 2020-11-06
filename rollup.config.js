a247aef5ad1ff38fd892ca3ba848cb2998b17234
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-11-06 14:22:26 -0800

    Simplifiy rollup, get external from peer deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,28 @@
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 
+const input = 'src/index.ts';
+const external = [...Object.keys(pkg.peerDependencies || {})];
+
 export default [
-  // this config applies settings for creating type declarations
   {
-    input: 'src/index.ts',
+    input,
+    external,
     output: {
-      dir: 'dist',
+      file: pkg.main,
+      format: 'cjs',
     },
-    external: ['vue'],
-    plugins: [
-      typescript({
-        declaration: true,
-        declarationDir: 'dist',
-        emitDeclarationOnly: true,
-        rootDir: 'src',
-      }),
-    ],
+    plugins: [typescript()],
   },
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: pkg.module,
-        format: 'es',
-      },
-      {
-        file: pkg.main,
-        format: 'cjs',
-      },
+    input,
+    external,
+    output: {
+      dir: 'dist',
+      format: 'es',
+    },
+    plugins: [
+      typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' }),
     ],
-    external: ['vue'],
-    plugins: [typescript()],
   },
 ];
